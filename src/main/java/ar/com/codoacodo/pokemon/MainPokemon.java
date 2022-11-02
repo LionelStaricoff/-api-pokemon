1b750e78836edb455c2b72214bfd842138e9e39a
lionel staricoff <staricofflionel@hotmail.com>
2022-11-02 16:50:51 -0500

    primer commit

---
+++ src/main/java/ar/com/codoacodo/pokemon/MainPokemon.java
@@ -0,0 +1,15 @@
+package ar.com.codoacodo.pokemon;
+
+public class MainPokemon {
+
+	
+	public static void main (String [] arg ) {
+		
+		
+		//https://pokeapi.co/api/v2/
+		// pat: pokemon/
+		
+	}
+	
+	
+}
