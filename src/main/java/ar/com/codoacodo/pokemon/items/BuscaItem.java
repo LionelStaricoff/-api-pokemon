c473dbe729abeb9910c9ac9055bb47a9e569f21d
lionel staricoff <staricofflionel@hotmail.com>
2023-03-16 01:09:12 -0500

    creando la conexion con buscaitem, fallo al crear el user del item, revisar el archivo yacarta

---
+++ src/main/java/ar/com/codoacodo/pokemon/items/BuscaItem.java
@@ -0,0 +1,53 @@
+package ar.com.codoacodo.pokemon.items;
+
+import ar.com.codoacodo.pokeapi.Item;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Invocation;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+public class BuscaItem {
+
+	private int numero;
+	
+	
+	public BuscaItem(int num) {
+		this.numero = num;
+	}
+
+
+	public Item findItem() {
+		//int random = (int) Math.floor(Math.random() *(905) +1);
+	
+		Client client =ClientBuilder.newClient();
+				
+				//https://reqres.in/api/users?page=2
+				WebTarget webTarget = client.target("https://pokeapi.co/api/v2/" )
+						.path("item/"+this.numero);//parametros que van despues de/
+						//.queryParam("page=2");// parametros que van despues de ? clave=valor & clave=valor
+				
+				//invocation
+				Invocation.Builder invocation= webTarget.request(MediaType.APPLICATION_JSON);
+				
+				//get
+				Response response = invocation.get();
+				int responseCode = response.getStatus();
+				//System.out.println("conectando"+responseCode);
+				//read
+				
+// problema creando el user, volver a bajara el 2pojo
+				Item user = response.readEntity(Item.class);
+				
+				return  user;
+			}
+	
+	public static void main (String args[]) {
+		BuscaItem bp = new BuscaItem(17);
+	Item pocion =	bp.findItem();
+	System.out.println(pocion.name);
+	System.out.println(pocion.url);
+	}
+	
+}
