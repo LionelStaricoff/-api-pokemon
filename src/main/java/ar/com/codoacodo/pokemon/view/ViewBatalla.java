f0cebc6189c1f621be242f2d5fe0bb8817872c37
lionel staricoff <staricofflionel@hotmail.com>
2023-08-30 20:40:07 -0500

    agregando funcionalidad del boton batalla y del cierre traidos del cherry-pick

--- src/main/java/ar/com/codoacodo/pokemon/view/ViewBatalla.java
+++ src/main/java/ar/com/codoacodo/pokemon/view/ViewBatalla.java
@@ -1,62 +1,267 @@
 package ar.com.codoacodo.pokemon.view;
 
-import java.awt.GridLayout;
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.io.File;
 
-import javax.swing.Icon;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 
-public class ViewBatalla extends JFrame {
+import ar.com.codoacodo.util.UtilVentana;
 
-	private JFrame ventana;
-	private JPanel panel;
-	private JPanel panelSuperior = new JPanel(new GridLayout(0,2, 100,0));
-	private JLabel jLabel;
-	private JLabel imgPokemonEnemy = new JLabel();
-	//private ImageIcon pokemonPng = new ImageIcon("/pokemon/src/main/java/ar/com/codoacodo/pokemon/img/fondoBatalla1.jpeg");
 
+
+public class ViewBatalla extends JFrame implements MouseListener{
+	
+	private JPanel panelAbuelo, panelPadre, panelSuperior,panelSuperiorIzquierdo,panelSuperMedio,
+	panelMedio,panelInferior,panelCerrar;
+     private JLabel btnSalir,batalla,items,cambioDePokemon;
+	
+ 
 	public ViewBatalla() {
-		this.ventana = new JFrame();
-		this.ventana.setLayout(new GridLayout(3,0));
-		this.ventana.setVisible(true);
-		this.ventana.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		//this.ventana.setIconImage(new ImageIcon("C:\\Users\\Lucia\\Documents\\lionel\\spring\\git\\pokemon\\-api-pokemon\\src\\main\\java\\ar\\com\\codoacodo\\pokemon\\img\\fondoBatalla1.jpeg").getImage());
-			
-		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		this.ventana.setSize(toolkit.getScreenSize());
+		
+	
+
+		/**
+		 * <p> Creando las ventanas de la batalla y agregando el panel abuelo base y
+		 * padre como fondo</p>
+		 * <p> primer panel divivdido en 2: hp pokemon1, label vacio y img pokemon2</p>
+		 */
+	
+		//panel padre
+		this.panelPadre = new JPanel (new GridLayout(3,0) );
+		UtilVentana.pintarImagenEnVentana(this.panelPadre,"C:/Users/Lucia/Documents/lionel/spring/git/pokemon/-api-pokemon/src/main/java/ar/com/codoacodo/pokemon/img/fondoBatalla1.jpeg");
+	    
+		//panel abuelo
+		this.panelAbuelo = new JPanel(new BorderLayout());
+		JPanel botonCerrar = new JPanel(new GridLayout(0,4));
+		botonCerrar.setBackground(Color.blue);
+		 this.btnSalir = new JLabel("x");
+		 this.btnSalir.setFont(new Font("Verdana", Font.PLAIN, 30));
+		 this.btnSalir.setOpaque(false);
+		 this.btnSalir.setHorizontalAlignment(SwingConstants.CENTER);
+		 this.btnSalir.setAlignmentX(JLabel.RIGHT_ALIGNMENT);
+		 this.btnSalir.addMouseListener(this);
+		botonCerrar.add(UtilVentana.ventanaVacia());
+		botonCerrar.add(UtilVentana.ventanaVacia());
+		botonCerrar.add(UtilVentana.ventanaVacia());
+		botonCerrar.add(btnSalir);
+		
+		this.panelAbuelo.add(botonCerrar,BorderLayout.NORTH);
+		this.panelAbuelo.add(this.panelPadre,BorderLayout.CENTER);
+		
+		
+		
+		//iniciando panel superior
+		this.panelSuperior = new JPanel(new GridLayout(0, 3) );
+		this.panelSuperior.setOpaque(false);
+		this.panelPadre.add( this.panelSuperior);
+		//iniciando panel medio
+		this.panelMedio = new JPanel(new GridLayout(0, 3) );
+		this.panelMedio.setOpaque(false);
+		this.panelPadre.add(this.panelMedio);
+		//panel inferior
+		this.panelInferior = new JPanel(new GridLayout(0, 3) );
+		this.panelInferior.setOpaque(false);
+		this.panelPadre.add(panelInferior);
+		
+		
+		
 		
 	
 		
-		this.panelSuperior.add(new JButton("vida pokemon 1"));
-		this.imgPokemonEnemy.setIcon( new ImageIcon("C:\\Users\\Lucia\\Documents\\lionel\\spring\\git\\pokemon\\-api-pokemon\\src\\main\\java\\ar\\com\\codoacodo\\pokemon\\img\\fondoBatalla1.jpeg"));
-			
-		this.panelSuperior.add(this.imgPokemonEnemy);
-		this.ventana.add( this.panelSuperior );
-		this.ventana.add(new JButton("boton 2"));
-		this.ventana.add(new JButton("boton 3"));
+		
+		/**
+		 * <p> setear  panel superior</p>
+		 * <p> primer panel divivdido en 3: hp pokemon1, label vacio y img pokemon2</p>
+		 */
+		this.panelSuperiorIzquierdo = UtilVentana.ventanaNombreHp("80 HP", "pikachu");
+		this.panelSuperior.add(this.panelSuperiorIzquierdo);
+		
+		
+		
+		//lb superior del medio vacio
+		this.panelSuperior.add( UtilVentana.ventanaVacia());
+		
+		// agregando panel superior izquierdo con la imagen
+	    this.panelSuperMedio = UtilVentana.ventanaCentrada(new ImageIcon("C:/Users/Lucia/Documents/lionel/spring/git/pokemon/-api-pokemon/src/test/java/img/pikachu.jpg"));
+		this.panelSuperior.add(this.panelSuperMedio);
+		
+		
+		
+		
+		
+		
+		/**
+		 * <p> setear segundo panel </p>
+		 * <p>segundo panel dividido en 3: imp pokemon1, label vacio y hp pokemon2</p>
+		 */
+		this.panelMedio.add(UtilVentana.ventanaCentrada(new ImageIcon("C:/Users/Lucia/Documents/lionel/spring/git/pokemon/-api-pokemon/src/test/java/img/pikachu.jpg")));
+		this.panelMedio.add(UtilVentana.ventanaVacia());
+		this.panelMedio.add(UtilVentana.ventanaNombreHp("hp pokemon2", "pikachu 2"));
+	
+		
+		
+		
+		
+		/**
+		 * <p> setear tercer panel </p>
+		 * <p>tercer panel dividido en 3: button batalla, button items y button cambio de pokemon </p>
+		 */
+		this.batalla = UtilVentana.crearLavelCentrado("batalla");
+	    this.batalla.addMouseListener(this);
+		this.panelInferior.add(UtilVentana.ventanaCentrada(this.batalla));
+		
+		this.items = UtilVentana.crearLavelCentrado("Items");
+		this.items.addMouseListener(this);
+		this.panelInferior.add(UtilVentana.ventanaCentrada(this.items));
+		
+		this.cambioDePokemon = UtilVentana.crearLavelCentrado("Cambio de pokemon");
+	    this.cambioDePokemon.addMouseListener(this);
+		this.panelInferior.add(UtilVentana.ventanaCentrada(this.cambioDePokemon));
+		this.panelInferior.setOpaque(false);
+		
+		
+		
 		
 	
-/*
-		panel = new JPanel();
-		this.ventana.add(panel);
-		jLabel = new JLabel("Fondo");
-		this.jLabel.setIcon(new ImageIcon("/-api-pokemon/src/main/java/ar/com/codoacodo/pokemon/img/fondoBatalla1.jpeg"));
+		
+		SwingUtilities.invokeLater(()->{
+		setSize(600,700);
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setUndecorated(true);
+		setVisible(true);
+		setContentPane(this.panelAbuelo);
+		revalidate();
+		repaint();
+		});
+		
+	}
 	
-		JButton button = new JButton();
-		button.setText("Button");
-		panel.add(jLabel);
-		panel.add(button);
-		this.ventana.add(panel);
-		*/
+	//ventana que setea los 4 ataques de los pokemon
+	public JPanel viewAtaques() {
+		JPanel panelPrincipal = new JPanel(new BorderLayout());
+	
+		
+		//crea un margin
+		panelPrincipal.setBorder(BorderFactory.createLoweredBevelBorder());
+		JPanel panelIzquierdo = new JPanel(new GridLayout(2,2,10,10));
+		this.panelCerrar = UtilVentana.ventanaCentrada("x     ");
+		panelCerrar.addMouseListener(this);
+		panelCerrar.setBorder(BorderFactory.createLineBorder(Color.RED));
+		
+		
+		panelIzquierdo.add(UtilVentana.crearLavelCentrado( "1") );
+		panelIzquierdo.add(UtilVentana.crearLavelCentrado( "2") );
+		panelIzquierdo.add(UtilVentana.crearLavelCentrado( "3") );
+		panelIzquierdo.add(UtilVentana.crearLavelCentrado( "4") );
+		
+		panelPrincipal.add(UtilVentana.ventanaVacia(),BorderLayout.WEST);
+		panelPrincipal.add(UtilVentana.ventanaVacia(),BorderLayout.SOUTH);
+		panelPrincipal.add(UtilVentana.ventanaVacia(),BorderLayout.NORTH);
+		panelPrincipal.add(panelIzquierdo,BorderLayout.CENTER);
+		panelPrincipal.add(panelCerrar,BorderLayout.EAST );
+		
+		panelCerrar.setOpaque(false);
+		panelIzquierdo.setOpaque(false);
+		panelPrincipal.setOpaque(false);
+		
+		return panelPrincipal;
+	}
+	
+   
+	
+	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if(	e.getSource() == btnSalir) System.exit(0);
+		if(e.getSource() == batalla) {
+			SwingUtilities.invokeLater(()->{
+			this.panelInferior.removeAll(); 
+			this.panelInferior.setLayout(new GridLayout(0, 1));
+			this.panelInferior.add(this.viewAtaques());
+			this.panelInferior.revalidate();
+			this.panelInferior.repaint();
+			});
+		}
+		if(e.getSource() == items) System.out.println("item");
+		if(e.getSource() == cambioDePokemon) System.out.println("cambio de pokemon");
+		if(e.getSource() == this.panelCerrar) {
+			SwingUtilities.invokeLater(()->{
+			this.panelInferior.removeAll(); 
+			this.panelInferior.setLayout(new GridLayout(0, 3));
+			this.panelInferior.add(UtilVentana.ventanaCentrada(this.batalla));
+			this.panelInferior.add(UtilVentana.ventanaCentrada(this.items));
+			this.panelInferior.add(UtilVentana.ventanaCentrada(this.cambioDePokemon));
+			this.panelInferior.revalidate();
+			this.panelInferior.repaint();
+			});
+		}
+		
 	}
 
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+		
+	}
+
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+		
+	}
+
+
+	//hover
+	public void mouseEntered(MouseEvent e) {
+		if(e.getSource() == batalla) {
+			this.batalla.setForeground(Color.BLUE);
+			this.batalla.setBorder(BorderFactory.createLineBorder(Color.BLUE));	
+		}
+		if(e.getSource() == items) {
+			this.items.setForeground(Color.BLUE);
+			this.items.setBorder(BorderFactory.createLineBorder(Color.BLUE));	
+		}
+		
+		if(e.getSource() == cambioDePokemon) {
+			this.cambioDePokemon.setForeground(Color.BLUE);
+			this.cambioDePokemon.setBorder(BorderFactory.createLineBorder(Color.BLUE));	
+		}
+		
+		
+	}
+
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		if(e.getSource() == batalla) {
+			this.batalla.setForeground(Color.RED);
+			this.batalla.setBorder(BorderFactory.createLineBorder(Color.RED));
+		}
+		if(e.getSource() == items) {
+			this.items.setForeground(Color.RED);
+			this.items.setBorder(BorderFactory.createLineBorder(Color.RED));
+
+		}
+	}
+
+
 	public static void main(String[] args) {
-		ViewBatalla vp = new ViewBatalla();
+
+		new ViewBatalla(); 
+
+
 	}
 }
---
+++ src/main/java/ar/com/codoacodo/util/UtilVentana.java
@@ -0,0 +1,185 @@
+package ar.com.codoacodo.util;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+
+
+public class UtilVentana  {
+
+	/**
+	 * <p>clase estatica para centrar componenetes dentro de ventanas BorderLayout
+	 * para colocar en hp del pokemon y en  nombre</p>
+	 * 
+	 * @param objeto de component
+	 * @return JPanel centrado
+	 */
+	public static JPanel ventanaNombreHp(String hp, String nombre) {
+
+		JPanel panelCentrado = new JPanel(new BorderLayout());
+
+		
+		panelCentrado.add(panelCentrado(nombre),BorderLayout.NORTH);
+		panelCentrado.add(new JLabel("    "),BorderLayout.EAST);
+		panelCentrado.add(panelCentrado(hp), BorderLayout.CENTER);
+		panelCentrado.add(new JLabel("     "),BorderLayout.SOUTH);
+		panelCentrado.add(new JLabel("             "),BorderLayout.WEST);
+
+
+		// crea transparencia al fondo
+		panelCentrado.setOpaque(false);
+
+		return panelCentrado;
+	}
+
+/**
+ * <p>Se coloca un string para pintar en la pantalla el nombre o hp del pokemon
+ * y que este centrado</p>
+ * @param String 
+ * @return JPanel
+ */
+	private static JPanel panelCentrado(String hp) {
+		JPanel panelHp = new JPanel(new FlowLayout());
+		panelHp.setAlignmentX(JPanel.CENTER_ALIGNMENT);
+		panelHp.setOpaque(false);
+		panelHp.add(crearLavelText(hp));
+		return panelHp;
+	}
+	
+	
+	public static JPanel ventanaCentrada(Component objeto) {
+
+		JPanel panelCentrado = new JPanel(new BorderLayout());
+
+		panelCentrado.add(new JLabel("   "),BorderLayout.NORTH);
+		panelCentrado.add(new JLabel("    "),BorderLayout.EAST);
+		panelCentrado.add( objeto, BorderLayout.CENTER);
+		panelCentrado.add(new JLabel("     "),BorderLayout.SOUTH);
+		panelCentrado.add(new JLabel("             "),BorderLayout.WEST);
+
+
+		// crea transparencia al fondo
+		panelCentrado.setOpaque(false);
+
+		return panelCentrado;
+	}
+	
+	
+	
+	/**
+	 * <p>clase estatica para centrar componenetes dentro de ventanas GridLayout</p>
+	 * 
+	 * @param objeto de component
+	 * @return JPanel centrado
+	 */
+	public static JPanel ventanaCentradaGridLayout(Object objeto) {
+
+		JPanel panelCentrado = new JPanel(new GridLayout(3, 3));
+
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add((Component) objeto);
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+		panelCentrado.add(new JLabel(""));
+
+		// crea transparencia al fondo
+		panelCentrado.setOpaque(false);
+
+		return panelCentrado;
+	}
+	
+	/**
+	 * <p> agregando un new ImageIcon("ruta de la imagen"); se carga al panel la imagen escalada</p>
+	 * @param icon
+	 * @return JPanel centrado y con la imagen escalada
+	 */
+	public static JPanel ventanaCentrada(ImageIcon icon) {
+		JPanel label = new JPanel();
+		try {
+			BgBorder borde = new BgBorder(ImageIO.read(new File(icon.getDescription())));
+			label.setBorder(borde);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return ventanaCentrada(label);
+	}
+	
+	/**
+	 * <p> setea una string con  las modificaciones del metodo crearLavelText()</p>
+	 * @param nombre del la etiqueta
+	 * @return JPanel
+	 */
+	public static JPanel ventanaCentrada(String nombre) {
+		return ventanaCentrada(crearLavelText( nombre));
+	}
+
+	
+	
+	/**
+	 * <p>Crea una ventana vacia para dar espacio entre los frames</p>
+	 * @return JPanel
+	 */
+	public static JPanel ventanaVacia() {
+		JPanel panel = new JPanel(new FlowLayout());
+		panel.setOpaque(false);
+		return panel;
+	}
+
+	
+	/**
+	 * <p>setea una imagen a la ventana sin perder dimencines</p>
+	 * @param JPanel ventana
+	 * @param url direccion del la  imagen
+	 */
+	public static void pintarImagenEnVentana(JPanel jPanel1, String url) {
+
+		try {
+			BgBorder borde = new BgBorder(ImageIO.read(new File(url)));
+			jPanel1.setBorder(borde);
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+
+	}
+	
+	public static JLabel crearLavelText(String nombre) {
+		JLabel lavel = new JLabel(nombre);
+		lavel.setForeground(Color.RED);
+		lavel.setFont(new Font("Tahoma", Font.BOLD, 20));
+		return lavel;
+	}
+	
+	public static JLabel crearLavelCentrado(String nombre) {
+		JLabel batalla = new JLabel(nombre);
+		  batalla.setOpaque(false);
+		 Border border = BorderFactory.createLineBorder(Color.RED);
+		 batalla.setForeground(Color.RED);
+		batalla.setBorder(border);
+		batalla.setHorizontalAlignment(SwingConstants.CENTER);
+		batalla.setFont(new Font("Tahoma", Font.BOLD, 20));
+		return batalla;
+	}
+	
+	
+	
+
+	
+	
+}
